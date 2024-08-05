e0ad13aa300f625e1e9feb10389e309f29602032
Sagnik Chakraborty <official.sagnikchakraborty2@gmail.com>
2024-08-05 16:17:26 +0530

    Creating Pages And Routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,34 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+
 const App = () => {
-  return <div className="text-3xl text-red-500">Hello React</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route path="/sign-in">
+            <SignIn />
+          </Route>
+          <Route path="/sign-up">
+            <SignUp />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
